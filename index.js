01baa1acd6350d718e4d828e30a9b8c7cdb2c538
Lucas Santos <lucsantosdev@gmail.com>
2024-07-05 20:22:16 -0300

    configs to take form inputs values and html-css files creation

--- index.js
+++ index.js
@@ -22,10 +22,37 @@ const db = mongoose.connection
 db.on('error', () => console.log("Error in Database connection."))
 db.once('open', () => console.log("Connected to Database."))
 
+app.post("/sign_up", (req, res) => {
+    const name = req.body.name
+    const age = req.body.age
+    const email = req.body.email
+    const phone = req.body.phone
+    const gender = req.body.gender
+    const password = req.body.password
+
+    const data = {
+        'name': name,
+        "age": age,
+        "email": email,
+        "phone": phone,
+        "gender":gender,
+        "password":password
+    }
+    //creating database collection
+    db.collection('users').insertOne(data,(err,collection) => {
+        if(err){
+            throw err
+        }
+        console.log("Record Inserted Successfully")
+    })
+    return res.redirect('signup_successful.html')
+})
+
 app.get("/", (req, res) => {
     res.set({
         "Allow-access-Allow-Origin":'*'
     })
+    return res.redirect('index.html')
 }).listen(3000)
 
 console.log("Listening on port 3000")
